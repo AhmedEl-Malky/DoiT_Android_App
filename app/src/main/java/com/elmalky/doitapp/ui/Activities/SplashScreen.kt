70b99a544b6a05c12f84ec746d5ce1abc21c170f
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-03-14 17:52:39 +0200

    handle first time to visit app with walkthrough by shared preferences

--- app/src/main/java/com/elmalky/doitapp/ui/Activities/SplashScreen.kt
+++ app/src/main/java/com/elmalky/doitapp/ui/Activities/SplashScreen.kt
@@ -1,18 +1,31 @@
 package com.elmalky.doitapp.ui.Activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.elmalky.doitapp.R
+import com.elmalky.doitapp.util.Constants
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed({
-            startActivity(Intent(this, WalkThroughActivity::class.java))
-            finish()
-        }, 1700)
+        val appSP = getSharedPreferences(Constants.Names.SP_NAME, Context.MODE_PRIVATE)
+//        appSP.edit().putBoolean(Constants.Names.FIRST_START,true).apply()
+        val firstTime = appSP.getBoolean(Constants.Names.FIRST_START, false)
+        if (firstTime) {
+            Handler().postDelayed({
+                startActivity(Intent(this, WalkThroughActivity::class.java))
+                appSP.edit().putBoolean(Constants.Names.FIRST_START, false).apply()
+                finish()
+            }, 1700)
+        } else {
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 1700)
+        }
     }
 }
